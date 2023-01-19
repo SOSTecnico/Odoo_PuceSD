84941111d0632faa6d46b1515aa5162a5cab3674
Jonathan Moreno <mario_morenor@hotmail.com>
2023-01-19 16:09:24 -0500

    Cambios HR

--- custom_addons/Hr/racetime/models/DetalleMarcaciones.py
+++ custom_addons/Hr/racetime/models/DetalleMarcaciones.py
@@ -1,7 +1,7 @@
 from psycopg2 import Error
 
 from odoo import fields, models, api
-import psycopg2
+import pyodbc
 
 from odoo.exceptions import ValidationError
 from odoo.http import request
@@ -12,26 +12,25 @@ class Biotime:
         try:
             config = self.obtener_parametros_conexion_biotime()
 
-            self.conexion = psycopg2.connect(
-                host=config['host'],
-                port=config['port'],
-                user=config['db_user'],
-                password=config['db_password'],
-                dbname=config['db_name']
-            )
+            self.conexion = pyodbc.connect(
+                'DRIVER={ODBC Driver 17 for SQL server}; SERVER=' + config['host'] +
+                ';DATABASE=' + config['db_name'] +
+                ';UID=' + config['db_user'] +
+                ';PWD=' + config['db_password'])
         except Error as ex:
             raise ValidationError(f"Ocurrió un error al conectarse a la Base de Datos: {ex}")
 
-    def execute_query(self,sql,parameters):
+    def obtener_marcaciones(self, sql):
         cursor = self.conexion.cursor()
-        cursor.execute(sql,parameters)
+        cursor.execute(sql)
         result = cursor.fetchall()
         self.conexion.commit()
         cursor.close()
         return result
 
+
     def obtener_parametros_conexion_biotime(self):
-        configuracion = request.env['racetime.config'].search_read([('key', 'like', 'radius')],
+        configuracion = request.env['racetime.config'].search_read([('key', 'like', 'biotime')],
                                                                    fields=['key', 'value'])
         config = {}
         for c in configuracion:
@@ -53,10 +52,23 @@ class DetalleMarcacion(models.Model):
     _description = 'Detalle Marcacion'
 
     name = fields.Char()
-    fecha_hora = fields.Datetime(string='Fecha y Hora', required=True)
+    fecha_hora = fields.Datetime(string='Fecha y Hora', required=False)
     id_empleado = fields.Integer(string='ID Empleado', required=False)
     empleado_id = fields.Many2one(comodel_name='hr.employee', string='Empleado', required=False)
-    
-    def obtener_marcaciones(self):
-        sql = """SELECT * FROM iclock_transaction WHERE """
-        Biotime.execute_query()
+
+
+
+    # def obtener_marcaciones(self):
+    #     sql = """SELECT * FROM iclock_transaction"""
+    #     result = Biotime().obtener_marcaciones(sql)
+    #     empleados = self.env['hr.employee'].search()
+    #
+    #     for row in result:
+    #         empleados.search([('emp_code')])
+    #         self.create({
+    #             'fecha_hora': row.punch_time,
+    #             'id_empleado':row.emp_code,
+    #             'empleado_id':
+    #
+    #         })
+    #         print(row.punch_time)
