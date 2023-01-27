0f54bb87b50f138febd81b3fb51262cad32a5790
unknown <jmmorenor@PUCESD.EDU.EC>
2023-01-27 18:11:20 -0500

    Cálculo de Horas

--- custom_addons/Hr/racetime/models/Horarios.py
+++ custom_addons/Hr/racetime/models/Horarios.py
@@ -11,8 +11,8 @@ class Horarios(models.Model):
     inicio_descanso = fields.Float(string='Inicio Descanso', required=False, tracking=True)
     fin_descanso = fields.Float(string='Fin Descanso', required=False, tracking=True)
     hora_fin = fields.Float(string='Hora Fin', required=True, tracking=True)
-    dias = fields.Char(string='Dias', required=True, tracking=True)
+    dias = fields.Char(string='Dias', required=False, tracking=True)
     fecha_inicio = fields.Date(string='Fecha Inicio', required=False)
     fecha_fin = fields.Date(string='Fecha Fin', required=False)
-
+    active = fields.Boolean(string='Active', required=False, default=True)
     empleados_ids = fields.Many2many(comodel_name='hr.employee', string='Empleados')
--- custom_addons/Hr/racetime/models/ReporteMarcaciones.py
+++ custom_addons/Hr/racetime/models/ReporteMarcaciones.py
@@ -1,4 +1,8 @@
+from datetime import datetime, timedelta
+import calendar
+
 from odoo import fields, models, api
+from odoo.exceptions import ValidationError
 
 
 class ReporteMarcacionesWizard(models.TransientModel):
@@ -10,8 +14,40 @@ class ReporteMarcacionesWizard(models.TransientModel):
     fecha_fin = fields.Date(string='Fecha Fin', required=True)
     empleados_ids = fields.Many2many(comodel_name='hr.employee', string='Empleados')
 
-    def prueba(self):
-        print("hello")
+    @api.onchange('fecha_inicio', 'fecha_fin')
+    def onchange_fechas(self):
+        if self.fecha_inicio == False:
+            self.fecha_inicio = datetime.now().replace(day=1)
+
+        if self.fecha_fin == False:
+            self.fecha_fin = datetime.now()
+
+    def calculo_marcaciones(self):
+        # Día:1 - Aquí yaceran todas las ilusiones de tener un sistema perfecto para calcular las horas y marcaciones
+        # de ingreso de los empleados, se presentarán inconvenientes y ocurrirán errores pero ahí vamos, con fé
+        # que de todas formas toca hacerlo... se irá actualizando este mensaje conforme avanza el proyecto.
+        fecha_ = self.fecha_inicio
+        horarios = self.env['racetime.horarios'].sudo().search([])
+        if not horarios:
+            raise ValidationError('Para realizar los cálculos por favor debe existir al menos un horario disponible')
+        while fecha_ <= self.fecha_fin:
+            for empleado in self.empleados_ids:
+                marcaciones_empleado = self.env['racetime.detalle_marcacion'].sudo().search(
+                    [('emp_code', '=', empleado.emp_code), ('fecha_hora', '>=', fecha_),
+                     ('fecha_hora', '<=', fecha_)], order="fecha_hora asc")
+
+                # Horarios disponibles de cada empleado
+                horarios_empleado = horarios.search_read([('empleados_ids', 'in', empleado.id)])
+                print(horarios_empleado)
+
+                for m in marcaciones_empleado:
+                    # Comparar las horas con los horarios
+                    pass
+                    # print(m.fecha_hora)
+            fecha_ = fecha_ + timedelta(days=1)
+
+        return
+
 
 class ReporteMarcaciones(models.Model):
     _name = 'racetime.reporte_marcaciones'
