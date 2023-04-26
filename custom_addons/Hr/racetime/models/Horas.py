500b1bb8ed22adb6391beb970e874da5061989f7
Jonathan Moreno <mario_morenor@hotmail.com>
2023-04-26 12:55:27 -0500

    Update. Se ha agregado un nuevo tipo a las horas para cargar datos de horas sin fechas

--- custom_addons/Hr/racetime/models/Horas.py
+++ custom_addons/Hr/racetime/models/Horas.py
@@ -9,12 +9,14 @@ class Horas(models.Model):
 
     name = fields.Char(related='empleado_id.name')
     empleado_id = fields.Many2one(comodel_name='hr.employee', string='Empleado', required=True, tracking=True)
-    fecha_desde = fields.Date(string='Fecha Inicio', required=True, tracking=True)
-    fecha_hasta = fields.Date(string='Fecha Hasta', required=True, tracking=True)
-    hora_desde = fields.Float(string='Hora Inicio', required=True, tracking=True)
-    hora_hasta = fields.Float(string='Hora Fin', required=True, tracking=True)
+    fecha_desde = fields.Date(string='Fecha Inicio', required=False, tracking=True)
+    fecha_hasta = fields.Date(string='Fecha Hasta', required=False, tracking=True)
+    hora_desde = fields.Float(string='Hora Inicio', required=False, tracking=True)
+    hora_hasta = fields.Float(string='Hora Fin', required=False, tracking=True)
+    total_horas = fields.Float(string='Total Horas', required=False)
     descripcion = fields.Text(string="Descripción", required=False, tracking=True)
-    tipo = fields.Selection(string='Tipo', required=True, selection=[('a_favor', 'A FAVOR'), ('extras', 'EXTRAS')],
+    tipo = fields.Selection(string='Tipo', required=True,
+                            selection=[('a_favor', 'A FAVOR'), ('extras', 'EXTRAS'), ('otros', 'OTROS')],
                             tracking=True)
     estado = fields.Selection(string='Estado', selection=[('pendiente', 'PENDIENTE'), ('aprobado', 'APROBADO'), ],
                               required=False, default='aprobado')
@@ -38,11 +40,13 @@ class Horas(models.Model):
         hora = self if self else values
 
         horas = hora.hora_hasta - hora.hora_desde
+        if hora.tipo == 'otros':
+            horas = hora.total_horas
 
         saldo = self.env['racetime.saldos'].search([('empleado_id', '=', hora.empleado_id.id)])
 
         detalle_saldo = self.env['racetime.detalle_saldos'].search([('horas_id', '=', hora.id)])
-        if hora.tipo == 'a_favor':
+        if hora.tipo == 'a_favor' or hora.tipo == 'otros':
             if not detalle_saldo:
                 saldo.write({
                     'detalle_saldos': [(0, 0, {
