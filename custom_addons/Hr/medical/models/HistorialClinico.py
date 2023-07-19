3ad9e0ad87d693133b7b7fb7dd9ff5b66cb0a81b
Jonathan Moreno <mario_morenor@hotmail.com>
2023-07-18 23:23:58 -0500

    Modulo del Centro Medico modificado vistas

--- custom_addons/Hr/medical/models/HistorialClinico.py
+++ custom_addons/Hr/medical/models/HistorialClinico.py
@@ -1,3 +1,5 @@
+import pytz
+
 from odoo import fields, models, api
 
 from datetime import datetime
@@ -17,11 +19,14 @@ class Consulta(models.Model):
 
     name = fields.Char()
 
-    fecha = fields.Date(string='Fecha', required=True, default=lambda self: datetime.now(), tracking=True)
-    motivo = fields.Text(string="Motivo", required=False, tracking=True)
+    fecha = fields.Date(string='Fecha', required=True,
+                        default=lambda self: datetime.now(pytz.timezone('America/Guayaquil')), tracking=True)
+    motivo = fields.Text(string="Motivo", required=True, tracking=True)
     diagnostico = fields.Text(string="Diagnóstico", required=False, tracking=True)
+    indicaciones = fields.Text(string="Indicaciones", required=False, tracking=True)
 
-    historia_id = fields.Many2one(comodel_name='medical.historia', string='Historia_id', required=False, tracking=True)
+    historia_id = fields.Many2one(comodel_name='medical.historia', string='Historia_id', required=False, tracking=True,
+                                  ondelete='cascade')
 
 
 class HistoriaClinica(models.Model):
@@ -29,9 +34,25 @@ class HistoriaClinica(models.Model):
     _description = 'Historia Clínica'
     _inherit = ['mail.thread', 'mail.activity.mixin']
 
-    name = fields.Char(string='Código')
+    def _codigo_historia(self):
+        ultima_historia = self.search([], order='codigo desc', limit=1)
+        if not ultima_historia:
+            return 1
+        else:
+            return ultima_historia.codigo + 1
+
+    def _codigo_str_historia(self):
+        ultima_historia = self.search([], order='codigo desc', limit=1)
+        return f"H-{str(ultima_historia.codigo + 1).zfill(5)}"
+
+    name = fields.Char(string='Código', default=_codigo_str_historia)
+    codigo = fields.Integer(string='Codigo', required=False, default=_codigo_historia)
+
     paciente_id = fields.Many2one(comodel_name='medical.paciente', string='Paciente', required=True, tracking=True)
-    antecedentes = fields.Text(string="Antecedentes personales de Salud", required=False, tracking=True)
+    paciente_cedula = fields.Char(string='Paciente Cedula', related='paciente_id.cedula', store=True)
+    antecedentes = fields.Text(string="Antecedentes personales", required=False, tracking=True)
     alergias_id = fields.Many2many(comodel_name='medical.alergias', string='Alergias', tracking=True)
     consultas = fields.One2many(comodel_name='medical.consulta', inverse_name='historia_id', string='Consultas',
                                 required=False, tracking=True)
+
+    active = fields.Boolean(string='Active', required=False, default=True)
--- custom_addons/Hr/medical/models/Paciente.py
+++ custom_addons/Hr/medical/models/Paciente.py
@@ -1,4 +1,5 @@
 from odoo import fields, models, api
+from datetime import datetime
 
 
 class Paciente(models.Model):
@@ -6,15 +7,35 @@ class Paciente(models.Model):
     _description = 'Paciente'
     _inherit = ['mail.thread', 'mail.activity.mixin']
 
-    name = fields.Char(compute='_compute_name', store=True)
+    name = fields.Char(compute='_compute_name', store=True, string="Nombre Completo")
 
     nombres = fields.Char(string='Nombres', required=True, tracking=True)
     apellidos = fields.Char(string='Apellidos', required=True, tracking=True)
     cedula = fields.Char(string='Cédula', required=True, tracking=True)
+    edad = fields.Integer(string='Edad', required=True)
     email = fields.Char(string='Email', required=False, tracking=True)
     celular = fields.Char(string='Celular', required=False, tracking=True)
+    fecha_nacimiento = fields.Date(string='Fecha de Nacimiento', required=False)
+
+    genero = fields.Selection(string='Genero', selection=[('m', 'Masculino'), ('f', 'Femenino'), ('o', 'Otros')],
+                              required=True)
+
+    tipo_sangre = fields.Selection(required=True, string='Tipo de Sangre',
+                                   selection=[('o_positivo', 'O+'),
+                                              ('o_negativo', 'O-'),
+                                              ('a_positivo', 'A+'),
+                                              ('a_negativo', 'A-'),
+                                              ('ab_positivo', 'AB+'),
+                                              ('ab_negativo', 'AB-'),
+                                              ])
 
     @api.depends('nombres', 'apellidos')
     def _compute_name(self):
         for paciente in self:
             paciente.name = f"{paciente.nombres} {paciente.apellidos}"
+
+    @api.onchange('fecha_nacimiento')
+    def _onchange_nacimiento(self):
+        if self.fecha_nacimiento:
+            self.edad = datetime.now().year - self.fecha_nacimiento.year
+
