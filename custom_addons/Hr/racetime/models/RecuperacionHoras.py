c23248c65db04d74be0b454abcd82950b9cb6e73
Jonathan Moreno <mario_morenor@hotmail.com>
2023-07-26 21:21:34 -0500

    Agregado Vista Y cambios en el modelo RecuperacionHoras

--- custom_addons/Hr/racetime/models/RecuperacionHoras.py
+++ custom_addons/Hr/racetime/models/RecuperacionHoras.py
@@ -12,14 +12,18 @@ class RecuperacionHoras(models.Model):
     _name = 'racetime.recuperacion_horas'
     _description = 'Recuperación Horas'
 
-    name = fields.Char()
+    name = fields.Char(related='empleado_id.name')
 
     fecha_envio_correo = fields.Date(string='Fecha Envío Correo', required=False)
     asignatura_id = fields.Many2one(comodel_name='racetime.asignaturas', string='Asignatura', required=True)
     paralelo = fields.Char(string='Paralelo', required=False)
     novedad = fields.Text(string="Novedad", required=True)
-    estado = fields.Selection(string='Estado', selection=[('a', 'APROBADO'), ('p', 'PENDIENTE'), ], default='p', )
+    estado = fields.Selection(string='Estado', selection=[('p', 'PENDIENTE'), ('a', 'APROBADO'), ], default='p', )
     active = fields.Boolean(string='Active', required=False, default=True)
     desde = fields.Datetime(string='Desde', required=True)
     hasta = fields.Datetime(string='Hasta', required=True)
-    empleado_id = fields.Many2one(comodel_name='hr.employee', string='Empleado', required=False)
+    empleado_id = fields.Many2one(comodel_name='hr.employee', string='Empleado', required=True)
+    fecha_inasistencia = fields.Date(string='Fecha Inasistencia', required=False)
+    marcacion_id = fields.Many2one(comodel_name='racetime.reporte_marcaciones', string='Marcacion',
+                                   required=True,
+                                   domain="[('empleado_id', '=', empleado_id),('observacion','=','sin_marcacion'),('fecha','=',fecha_inasistencia)]")
