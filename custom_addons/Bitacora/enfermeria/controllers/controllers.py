80126ce8d0b45ed784e74a5757de2eb1581486b3
Jonathan Moreno <mario_morenor@hotmail.com>
2023-11-17 11:34:29 -0500

    Formulario Enferemeria

--- custom_addons/Bitacora/enfermeria/controllers/controllers.py
+++ custom_addons/Bitacora/enfermeria/controllers/controllers.py
@@ -5,8 +5,26 @@ from odoo.http import request
 
 class Enfermeria(http.Controller):
     @http.route('/enfermeria/formulario-asistencia', auth='public', website=True)
-    def formulario(self, **kw):
-        return request.render('enfermeria.formulario_asistencia_template')
+    def formulario(self, **data):
+        print(data)
+        if 'cedula' in data:
+            usuario = request.env['estudiantes.estudiantes'].sudo().search([('cedula', '=', data['cedula'])])
+            carreras = request.env['enfermeria.carreras'].sudo().search([])
+
+            asignaturas = request.env['enfermeria.asignaturas'].sudo().search([])
+            laboratorios = request.env['enfermeria.laboratorios'].sudo().search([])
+
+            return request.render('enfermeria.formulario_asistencia_template', {
+                'usuario': usuario,
+                'carreras': carreras,
+                'asignaturas': asignaturas,
+                'laboratorios': laboratorios
+            })
+        return self.formulario_verificar()
+
+    @http.route('/enfermeria/formulario-asistencia/verificar', auth='public', website=True)
+    def formulario_verificar(self, **kw):
+        return request.render('enfermeria.formulario_asistencia_verificar_template')
 
 #     @http.route('/enfermeria/enfermeria/objects', auth='public')
 #     def list(self, **kw):
