5e12adc3bf11f87f176bdf174ffe52f240140d27
Jonathan Moreno <mario_morenor@hotmail.com>
2023-11-21 08:27:20 -0500

    Biometricos ESIS

--- custom_addons/Bitacora/rutas/models/Chofer.py
+++ custom_addons/Bitacora/rutas/models/Chofer.py
@@ -9,10 +9,10 @@ class Chofer(models.Model):
     name = fields.Char(string='Nombre Completo', compute='_compute_name', store=True)
     nombres = fields.Char(string='Nombres', required=True, tracking=True)
     apellidos = fields.Char(string='Apellidos', required=True, tracking=True)
-    cedula = fields.Char(string='Cédula', required=True,tracking=True)
-    correo = fields.Char(string='Correo', required=True,tracking=True)
+    cedula = fields.Char(string='Cédula', required=True, tracking=True)
+    correo = fields.Char(string='Correo', required=True, tracking=True)
+    password = fields.Char(string='Contraseña')
     active = fields.Boolean(string='Active', required=False, default=True)
-
 
     @api.depends('nombres', 'apellidos')
     def _compute_name(self):
