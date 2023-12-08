7e74d49fef5a3f768ee4c56583252d5e857a102f
Jonathan Moreno <mario_morenor@hotmail.com>
2023-12-08 17:14:02 -0500

    Agregado QR al metodo check usuario para biblioteca

--- custom_addons/Bitacora/banner/controllers/controllers.py
+++ custom_addons/Bitacora/banner/controllers/controllers.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 from odoo import http
 from odoo.http import request
+import qrcode
+from io import BytesIO
+import base64
 
 
 class Banner(http.Controller):
@@ -10,12 +13,23 @@ class Banner(http.Controller):
         usuario = request.env['banner.usuarios'].sudo().search([('cedula', '=', data['cedula'])])
 
         if usuario:
-            datos = {
+
+            qr = qrcode.QRCode(version=1, error_correction=qrcode.constants.ERROR_CORRECT_L, box_size=20, border=4)
+            info = {
                 'cedula': usuario.read()[0]['cedula'],
                 'nombres': usuario.read()[0]['nombres'],
                 'apellidos': usuario.read()[0]['apellidos'],
                 'correo': usuario.read()[0]['correo'],
             }
-            return datos
+
+            qr.add_data(info)
+            qr.make(fit=True)
+            temp = BytesIO()
+            img = qr.make_image()
+            img.save(temp)
+            info.update({
+                "image": base64.b64encode(temp.getvalue())
+            })
+            return info
         else:
             return {}
